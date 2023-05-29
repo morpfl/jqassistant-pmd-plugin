d6ecd85480ee55a5bbfa925370be80cda1efef0c
Moritz Pflügner <moritz.pfluegner45@gmail.com>
2023-05-29 17:52:41 +0200

    fixed pom xml, added descriptor interface, usage of configuration class instead of parameter array

---
+++ src/main/java/org/jqassistant/plugin/pmd/model/PmdReport.java
@@ -0,0 +1,7 @@
+package org.jqassistant.plugin.pmd.model;
+
+import com.buschmais.jqassistant.core.store.api.model.Descriptor;
+import com.buschmais.jqassistant.plugin.common.api.model.NamedDescriptor;
+
+public interface PmdReport extends Descriptor, NamedDescriptor {
+}
--- src/main/java/org/jqassistant/plugin/pmd/scanner/PMDScannerPlugin.java
+++ src/main/java/org/jqassistant/plugin/pmd/scanner/PMDScannerPlugin.java
@@ -5,21 +5,41 @@ import com.buschmais.jqassistant.core.scanner.api.Scanner;
 import com.buschmais.jqassistant.core.scanner.api.Scope;
 import com.buschmais.jqassistant.core.store.api.model.Descriptor;
 import com.buschmais.jqassistant.plugin.common.api.scanner.AbstractScannerPlugin;
+import lombok.extern.slf4j.Slf4j;
 import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.PMDConfiguration;
+import net.sourceforge.pmd.PmdAnalysis;
+import org.jqassistant.plugin.pmd.model.PmdReport;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.format.TextStyle;
-
-public class PMDScannerPlugin extends AbstractScannerPlugin {
+@Slf4j
+public class PMDScannerPlugin extends AbstractScannerPlugin<PmdReport, PmdReport> {
     @Override
-    public boolean accepts(Object o, String s, Scope scope) throws IOException {
+    public boolean accepts(PmdReport pmdReport, String s, Scope scope) throws IOException {
         return true;
     }
 
     @Override
-    public Descriptor scan(Object o, String s, Scope scope, Scanner scanner) throws IOException {
-        String[] pmdArgs = new String[0];
-        PMD.main(pmdArgs);
+    protected void configure() {
+        log.info("Performing PMD analysis.");
+        File pmdReportFile = new File(System.getProperty("user.dir") + "/pmd.xml");
+        if(pmdReportFile.exists() && !pmdReportFile.isDirectory()){
+            log.info("PMD analysis already been performed.");
+            return;
+        }
+        PMDConfiguration configuration = new PMDConfiguration();
+        configuration.addRuleSet(System.getProperty("user.dir") + "/jqassistant-pmd-metrics.xml");
+        configuration.setInputPaths(System.getProperty("user.dir"));
+        configuration.setReportFormat("xml");
+        configuration.setReportFile(System.getProperty("user.dir") + "/pmd.xml");
+        PmdAnalysis pmd = PmdAnalysis.create(configuration);
+        pmd.performAnalysis();
+    }
+
+    @Override
+    public PmdReport scan(PmdReport pmdReport, String s, Scope scope, Scanner scanner) throws IOException {
         return null;
     }
 }
